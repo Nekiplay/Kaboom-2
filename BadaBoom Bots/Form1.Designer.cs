24e25ab1b7f1a5a105d7d2e3816b974170846a52
Neki_play <neci.03@mail.ru>
2021-01-22 20:37:37 +0500

    Добавьте файлы проекта.

---
+++ BadaBoom Bots/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace BadaBoom_Bots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bunifuCheckbox1 = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bunifuCheckbox2 = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuCheckbox1
+            // 
+            this.bunifuCheckbox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox1.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox1.Checked = true;
+            this.bunifuCheckbox1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCheckbox1.Location = new System.Drawing.Point(7, 21);
+            this.bunifuCheckbox1.Name = "bunifuCheckbox1";
+            this.bunifuCheckbox1.Size = new System.Drawing.Size(20, 20);
+            this.bunifuCheckbox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Звуковое уведомление";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AFK режим";
+            // 
+            // bunifuCheckbox2
+            // 
+            this.bunifuCheckbox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox2.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox2.Checked = false;
+            this.bunifuCheckbox2.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox2.ForeColor = System.Drawing.Color.White;
+            this.bunifuCheckbox2.Location = new System.Drawing.Point(7, 47);
+            this.bunifuCheckbox2.Name = "bunifuCheckbox2";
+            this.bunifuCheckbox2.Size = new System.Drawing.Size(20, 20);
+            this.bunifuCheckbox2.TabIndex = 2;
+            this.bunifuCheckbox2.OnChange += new System.EventHandler(this.bunifuCheckbox2_OnChange);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.bunifuCheckbox2);
+            this.panel1.Controls.Add(this.bunifuCheckbox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 96);
+            this.panel1.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(3, 4);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 14);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Chat-Game";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(207, 132);
+            this.panel3.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(10, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 14);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Dragon Glory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 68);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "E";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 14);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Disk:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 132);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kaboom  2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCheckbox bunifuCheckbox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuCheckbox bunifuCheckbox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BadaBoom Bots/Form1.cs
@@ -0,0 +1,181 @@
+﻿using Gma.System.MouseKeyHook;
+using Process.NET;
+using Process.NET.Memory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadaBoom_Bots
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> all = new List<string>();
+        List<string> newst = new List<string>();
+        long oldlenght = 0;
+        static double otver = -1;
+        List<string> newstrs = new List<string>();
+        private void Worker()
+        {
+            while (true)
+            {
+                string path = textBox1.Text + @":\Users\" + Environment.UserName + @"\AppData\Roaming\Kaboom\modpacks\1.7.10\modpacks\dragonglory\logs\latest.log";
+                if (File.Exists(path))
+                {
+                    using (FileStream sr = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        using (StreamReader file = new StreamReader(sr, Encoding.Default))
+                        {
+                            String line;
+                            long lenght = 0;
+                            all.Clear();
+                            while ((line = file.ReadLine()) != null) //читаем по одной линии(строке) пока не вычитаем все из потока (пока не достигнем конца файла)
+                            {
+                                lenght++;
+                                all.Add(line);
+                                if (all.Count() > newst.Count())
+                                {
+                                    newst.Add(line);
+                                    newstrs.Add(line);
+                                }
+                            }
+                            if (oldlenght == 0)
+                                oldlenght = lenght;
+                            if (oldlenght < lenght)
+                            {
+                                for (long i = oldlenght; i < lenght; i++)
+                                {
+                                    string doneline = (newst[(int)i]);
+                                    if (doneline.Contains("[Kaboom] Решите пример:"))
+                                    {
+                                        string expr = Regex.Match(doneline, "Решите пример: (.*)").Groups[1].Value;
+                                        double tx = Calculator.Calc(expr);
+                                        otver = tx;
+                                        if (bunifuCheckbox1.Checked)
+                                        {
+                                            new Thread(() =>
+                                            {
+                                                SoundPlayer player = new SoundPlayer("25.wav");//В скобках имя файла в формате wav
+                                            player.Play();
+                                            }).Start();
+                                        }
+                                        Console.WriteLine("Ответ: " + tx);
+                                        if (bunifuCheckbox2.Checked)
+                                        {
+                                            new Thread(() =>
+                                            {
+                                                Thread.Sleep(250);
+                                                SendCalc();
+                                            }).Start();
+                                        }
+                                    }
+                                    else if (doneline.Contains("[Kaboom] Победил игрок"))
+                                    {
+                                        string expr = Regex.Match(doneline, "Победил игрок (.*)").Groups[1].Value;
+                                        Console.WriteLine("Победил игрок: " + expr);
+                                        otver = -1;
+                                    }
+                                }
+                                oldlenght = lenght;
+                            }
+                        }
+                    }
+                    Thread.Sleep(25);
+                }
+            }
+        }
+        private void SendCalc()
+        {
+            System.Diagnostics.Process game = FindGame("javaw", "Minecraft");
+            if (game != null)
+            {
+                var process = new ProcessSharp(game, MemoryType.Remote);
+                process.WindowFactory.MainWindow.Keyboard.Press(Process.NET.Native.Types.Keys.T);
+                Thread.Sleep(16);
+                process.WindowFactory.MainWindow.Keyboard.Release(Process.NET.Native.Types.Keys.T);
+                Thread.Sleep(16);
+                process.WindowFactory.MainWindow.Keyboard.Press(Process.NET.Native.Types.Keys.Back);
+                Thread.Sleep(16);
+                process.WindowFactory.MainWindow.Keyboard.Release(Process.NET.Native.Types.Keys.Back);
+                Thread.Sleep(25);
+                foreach (char st in otver.ToString().ToArray())
+                {
+                    //Console.WriteLine(st.ToString());
+                    Process.NET.Native.Types.Keys press = Process.NET.Native.Types.Keys.None;
+                    Enum.TryParse("D" + st.ToString().ToUpper(), out press);
+                    if (press != Process.NET.Native.Types.Keys.None)
+                    {
+                        process.WindowFactory.MainWindow.Keyboard.Press(press);
+                        Thread.Sleep(22);
+                        process.WindowFactory.MainWindow.Keyboard.Release(press);
+                        Thread.Sleep(22);
+                    }
+                }
+                Thread.Sleep(35);
+                process.WindowFactory.MainWindow.Keyboard.PressRelease(Process.NET.Native.Types.Keys.Enter);
+
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Worker);
+            th.Start();
+
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyUp += GlobalHookKeyPress;
+        }
+        public System.Diagnostics.Process FindGame(string process, string title)
+        {
+            System.Diagnostics.Process[] javas = System.Diagnostics.Process.GetProcessesByName(process);
+            foreach (System.Diagnostics.Process java in javas)
+            {
+                if (java.MainWindowTitle.Contains(title))
+                {
+                    return java;
+                }
+            }
+            return null;
+        }
+        private void GlobalHookKeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                Console.WriteLine("Pressed " + otver);
+                if (otver != -1)
+                {
+                    SendCalc();
+                }
+            }
+        }
+        private void bunifuCheckbox2_OnChange(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public static class Calculator
+    {
+        // Создадим статический экземпляр DataTable, чтобы каждый раз не инициализировать его заново
+        private static DataTable Table { get; } = new DataTable();
+
+        // Наш метод подсчета
+        // Добавьте отлов ошибок по вкусу)
+        public static double Calc(string Expression) => Convert.ToDouble(Table.Compute(Expression, string.Empty));
+    }
+}
